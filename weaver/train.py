38844b79759144cc8227c2d6e6bc84fff7cd995d
Raffaele <raffaele.gerosa@cern.ch>
2022-05-23 13:10:56 +0200

    bug fix

--- weaver/train.py
+++ weaver/train.py
@@ -748,7 +748,7 @@ def _main(args):
         elif args.backend is None:
             if gpus is not None and len(gpus) > 1:
                 #model = torch.nn.DataParallel(model, device_ids=gpus)
-                model = torch.nn.parallel.DistributedDataParallel(model, device_ids=gpus)
+                model = torch.nn.parallel.DistributedDataParallel(model, device_ids=gpus, output_device=local_rank)
 
         # optimizer & learning rate
         opt, scheduler = optim(args, model, dev)
