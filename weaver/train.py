5cb4a2385a882ad5869b09bd3e69413c77aa1f5c
Raffaele <raffaele.gerosa@cern.ch>
2022-05-24 20:46:10 +0200

    adding disabling of pin memory

--- weaver/train.py
+++ weaver/train.py
@@ -241,10 +241,10 @@ def train_load(args):
                                  infinity_mode=args.steps_per_epoch_val is not None,
                                  in_memory=args.in_memory,
                                  name='val' + ('' if args.local_rank is None else '_rank%d' % args.local_rank))
-    train_loader = DataLoader(train_data, batch_size=args.batch_size, drop_last=True, pin_memory=args.disable_pin_memeory,
+    train_loader = DataLoader(train_data, batch_size=args.batch_size, drop_last=True, pin_memory=(not args.disable_pin_memeory),
                               num_workers=min(args.num_workers, int(len(train_files) * args.file_fraction)),
                               persistent_workers=args.num_workers > 0 and args.steps_per_epoch is not None)
-    val_loader = DataLoader(val_data, batch_size=args.batch_size, drop_last=True, pin_memory=args.disable_pin_memeory,
+    val_loader = DataLoader(val_data, batch_size=args.batch_size, drop_last=True, pin_memory=(not args.disable_pin_memeory),
                             num_workers=min(args.num_workers, int(len(val_files) * args.file_fraction)),
                             persistent_workers=args.num_workers > 0 and args.steps_per_epoch_val is not None)
     data_config = train_data.config
