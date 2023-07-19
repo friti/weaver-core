d96f40d4ba5ec07063a70354db29fd1bdc390daa
Raffaele <raffaele.gerosa@cern.ch>
2023-07-19 13:05:06 +0200

    fixing issue with onnx evaluation

--- weaver/utils/nn/tools_domain.py
+++ weaver/utils/nn/tools_domain.py
@@ -198,7 +198,7 @@ def train_classreg(model, loss_func, opt, scheduler, train_loader, dev, epoch, s
             model_output_reg = model_output_reg.detach()
             model_output_domain = model_output_domain.detach()
             
-            if torch.is_tensor(label_cat) and torch.is_tensor(model_output_cat) and np.iterable(label_cat) and np.iterable(model_output_cat):
+            if torch.is_tensor(label_cat) and torch.is_tensor(model_output_cat) and np.iterable(label_cat) and np.iterable(model_output_cat) and model_output_cat.nelement():
                 _, pred_cat = model_output_cat.max(1);
                 pred_reg = model_output_reg.float();
                 if pred_cat.shape == label_cat.shape and pred_reg.shape == target.shape:
@@ -211,7 +211,7 @@ def train_classreg(model, loss_func, opt, scheduler, train_loader, dev, epoch, s
 
             ## single domain region
             if num_domains == 1:
-                if torch.is_tensor(label_domain) and torch.is_tensor(model_output_domain) and np.iterable(label_domain) and np.iterable(model_output_domain):
+                if torch.is_tensor(label_domain) and torch.is_tensor(model_output_domain) and np.iterable(label_domain) and np.iterable(model_output_domain) and model_output_domain.nelement():
                     _, pred_domain = model_output_domain.max(1);
                     if pred_domain.shape == label_domain.shape:
                         correct_domain = (pred_domain == label_domain).sum().item()
@@ -225,7 +225,7 @@ def train_classreg(model, loss_func, opt, scheduler, train_loader, dev, epoch, s
                     label  = label_domain[index_domain[k],idx].squeeze()
                     if not torch.is_tensor(label) or not np.iterable(label): continue;
                     pred_domain = model_output_domain[index_domain[k],id_dom:id_dom+ldomain[idx]].squeeze();
-                    if not torch.is_tensor(pred_domain) or not np.iterable(pred_domain): continue;
+                    if not torch.is_tensor(pred_domain) or not np.iterable(pred_domain) or not model_output_domain.nelement(): continue;
                     _, pred_domain = pred_domain.max(1);
                     if pred_domain.shape != label.shape: continue;
                     correct_domain += (pred_domain == label).sum().item()
@@ -523,7 +523,7 @@ def evaluate_classreg(model, test_loader, dev, epoch, for_training=True, loss_fu
                 total_domain_loss += loss_domain
 
                 ## prediction + metric for classification
-                if np.iterable(label_cat) and torch.is_tensor(label_cat) and np.iterable(model_output_cat) and torch.is_tensor(model_output_cat):
+                if np.iterable(label_cat) and torch.is_tensor(label_cat) and np.iterable(model_output_cat) and torch.is_tensor(model_output_cat) and model_output_cat.nelement():
                     _, pred_cat = model_output_cat.max(1);
                     pred_reg = model_output_reg.float();
                     if pred_cat.shape == label_cat.shape and pred_reg.shape == target.shape:
@@ -536,7 +536,7 @@ def evaluate_classreg(model, test_loader, dev, epoch, for_training=True, loss_fu
 
                 ## single domain region                                                                                                                                                          
                 if num_domains == 1:
-                    if torch.is_tensor(label_domain) and torch.is_tensor(model_output_domain) and np.iterable(label_domain) and np.iterable(model_output_domain):
+                    if torch.is_tensor(label_domain) and torch.is_tensor(model_output_domain) and np.iterable(label_domain) and np.iterable(model_output_domain) and model_output_domain.nelement():
                         _, pred_domain = model_output_domain.max(1);
                         if pred_domain.shape == label_domain.shape:
                             correct_domain = (pred_domain == label_domain).sum().item()
@@ -550,7 +550,7 @@ def evaluate_classreg(model, test_loader, dev, epoch, for_training=True, loss_fu
                         label = label_domain[index_domain[k],idx].squeeze()
                         if not torch.is_tensor(label) or not np.iterable(label): continue;
                         pred_domain = model_output_domain[index_domain[k],id_dom:id_dom+ldomain[idx]].squeeze()
-                        if not torch.is_tensor(pred_domain) or not np.iterable(pred_domain): continue;
+                        if not torch.is_tensor(pred_domain) or not np.iterable(pred_domain) or not model_output_domain.nelement(): continue;
                         _, pred_domain = pred_domain.max(1);
                         if pred_domain.shape != label.shape: continue;
                         correct_domain += (pred_domain == label).sum().item()
@@ -676,12 +676,11 @@ def evaluate_onnx_classreg(model_path, test_loader,
 
     data_config = test_loader.dataset.config
     label_cat_counter = Counter()
-    total_loss, num_batches, total_cat_correct, total_domain_correct, sum_sqr_err, count_cat, count_domain = 0, 0, 0, 0, 0, 0, 0;
+    total_loss, num_batches, total_cat_correct, sum_sqr_err, count_cat, count_domain = 0, 0, 0, 0, 0, 0;
     labels_cat, labels_domain, targets, observers = defaultdict(list), defaultdict(list), defaultdict(list), defaultdict(list)
     scores_cat, scores_reg, scores_domain = [], [], []
-    scores_domain  = defaultdict(list); 
-    pred_cat, pred_domain, pred_reg = None, None, None;
-    inputs, label_cat, label_domain, target, model_output, model_output_cat, model_output_reg, model_output_domain  = None, None, None, None, None , None, None, None;
+    pred_cat, pred_reg = None, None;
+    inputs, label_cat, label_domain, target, model_output, model_output_cat, model_output_reg =  None, None, None, None , None, None, None;
     indexes_cat = [];
     indexes_domain = defaultdict(list); 
     index_offset = 0;
@@ -795,35 +794,25 @@ def evaluate_onnx_classreg(model_path, test_loader,
             model_output = torch.as_tensor(np.array(model_output)).squeeze();            
             model_output_cat = model_output[:,:num_labels]
             model_output_reg = model_output[:,num_labels:num_labels+num_targets];
-            model_output_domain = model_output[:,num_labels+num_targets:num_labels+num_targets+num_labels_domain];
             model_output_cat = _flatten_preds(model_output_cat,None)
             label_cat = label_cat.squeeze();
             label_domain = label_domain.squeeze();
             target = target.squeeze();
-
             scores_cat.append(torch.softmax(model_output_cat,dim=1).cpu().numpy().astype(dtype=np.float32));
             scores_reg.append(model_output_reg.cpu().numpy().astype(dtype=np.float32));
 
-            for idx, name in enumerate(y_domain.keys()):
-                id_dom = idx*ldomain[idx];
-                score_domain = model_output_domain[:,id_dom:id_dom+ldomain[idx]];
-                scores_domain[name].append(torch.softmax(score_domain.squeeze(),dim=1).cpu().numpy().astype(dtype=np.float32));
-            
             model_output_cat = model_output_cat[index_cat];
             model_output_reg = model_output_reg[index_cat];
-            model_output_domain = model_output_domain[index_domain_all];                        
-
             ### adjsut outputs
             model_output_cat = model_output_cat.squeeze().float();
             model_output_reg = model_output_reg.squeeze().float();
-            model_output_domain = model_output_domain.squeeze().float();
 
             num_batches += 1
             index_offset += (num_cat_examples+num_domain_examples)
-
+            if num_batches >= 5: break;
             ## prediction + metric for classification
-            if np.iterable(label_cat) and torch.is_tensor(label_cat) and np.iterable(model_output_cat) and torch.is_tensor(model_output_cat):
-                _, pred_cat = model_output_cat.max(1);
+            if np.iterable(label_cat) and torch.is_tensor(label_cat) and np.iterable(model_output_cat) and torch.is_tensor(model_output_cat) and model_output_cat.nelement():
+                _, pred_cat = model_output_cat.detach().max(1);
                 correct_cat = (pred_cat == label_cat).sum().item()
                 count_cat += num_cat_examples
                 total_cat_correct += correct_cat
@@ -832,33 +821,12 @@ def evaluate_onnx_classreg(model_path, test_loader,
                 residual_reg = pred_reg - target;
                 sqr_err = residual_reg.square().sum().item()
                 sum_sqr_err += sqr_err
-
-            ## single domain region                                                                                                                                                          
-            if num_domains == 1:
-                if np.iterable(label_domain) and torch.is_tensor(label_domain) and np.iterable(model_output_domain) and torch.is_tensor(model_output_cat):
-                    _, pred_domain = model_output_domain.detach().max(1);
-                    correct_domain = (pred_domain == label_domain).sum().item()
-                    total_domain_correct += correct_domain
-                    count_domain += num_domain_examples                
-                ## multiple domains
-            else:
-                correct_domain = 0;
-                for idx, (k,v) in enumerate(y_domain_check.items()):
-                    id_dom = idx*ldomain[idx];
-                    label = label_domain[index_domain[k],idx].squeeze()
-                    pred_domain = model_output_domain[index_domain[k],id_dom:id_dom+ldomain[idx]].squeeze()
-                    if np.iterable(label) and torch.is_tensor(label) and np.iterable(pred_domain) and torch.is_tensor(pred_domain):
-                        _, pred_domain = pred_domain.max(1);
-                        correct_domain += (pred_domain == label).sum().item()
-                total_domain_correct += correct_domain
-                count_domain += num_domain_examples                
+            count_domain += num_domain_examples;
                 
             ### monitor results
             tq.set_postfix({
                 'AccCat': '%.5f' % (correct_cat / num_cat_examples if num_cat_examples else 0),
                 'AvgAccCat': '%.5f' % (total_cat_correct / count_cat if count_cat else 0),
-                'AccDomain': '%.5f' % (correct_domain / num_domain_examples if num_domain_examples else 0),
-                'AvgAccDomain': '%.5f' % (total_domain_correct / count_domain if count_domain else 0),
                 'MSE': '%.5f' % (sqr_err / num_cat_examples if num_cat_examples else 0),
                 'AvgMSE': '%.5f' % (sum_sqr_err / count_cat if count_cat else 0),
             })
@@ -866,31 +834,23 @@ def evaluate_onnx_classreg(model_path, test_loader,
     time_diff = time.time() - start_time
     _logger.info('Processed %d entries in total (avg. speed %.1f entries/s)' % (count_cat+count_domain, (count_cat+count_domain) / time_diff))
     _logger.info('Eval AvgAccCat: %.5f'%(total_cat_correct / count_cat if count_cat else 0))
-    _logger.info('Eval AvgAccDomain: %.5f'%(total_domain_correct / (count_domain) if count_domain else 0))
     _logger.info('Eval AvgMSE: %.5f'%(sum_sqr_err / count_cat if count_cat else 0))
     _logger.info('Evaluation class distribution: \n    %s', str(sorted(label_cat_counter.items())))
     _logger.info('Train domain distribution: \n %s', ' '.join([str(sorted(i.items())) for i in label_domain_counter]))
     
     scores_cat = np.concatenate(scores_cat).squeeze()
     scores_reg = np.concatenate(scores_reg).squeeze()
-    scores_domain = {k: _concat(v) for k, v in scores_domain.items()}
     indexes_cat = np.concatenate(indexes_cat).squeeze()
     indexes_domain = {k: _concat(v) for k, v in indexes_domain.items()}
     labels_cat  = {k: _concat(v) for k, v in labels_cat.items()}
     labels_domain  = {k: _concat(v) for k, v in labels_domain.items()}
     targets = {k: _concat(v) for k, v in targets.items()}
     observers = {k: _concat(v) for k, v in observers.items()}
-
+    
     metric_cat_results = evaluate_metrics(labels_cat[data_config.label_names[0]][indexes_cat].squeeze(),scores_cat[indexes_cat].squeeze(),eval_metrics=eval_cat_metrics)            
     _logger.info('Evaluation Classification metrics: \n%s', '\n'.join(
         ['    - %s: \n%s' % (k, str(v)) for k, v in metric_cat_results.items()]))
 
-    for idx, (name,element) in enumerate(labels_domain.items()):
-        metric_domain_results = evaluate_metrics(element[indexes_domain[name]].squeeze(),scores_domain[name][indexes_domain[name]].squeeze(),eval_metrics=eval_cat_metrics)
-        _logger.info('Evaluation Domain metrics for '+name+' : \n%s', '\n'.join(
-            ['    - %s: \n%s' % (k, str(v)) for k, v in metric_domain_results.items()]))
-        
-        
     for idx, (name,element) in enumerate(targets.items()):
         if num_targets == 1:
             metric_reg_results = evaluate_metrics(element[indexes_cat].squeeze(), scores_reg[indexes_cat].squeeze(), eval_metrics=eval_reg_metrics)
@@ -900,8 +860,7 @@ def evaluate_onnx_classreg(model_path, test_loader,
                 ['    - %s: \n%s' % (k, str(v)) for k, v in metric_reg_results.items()]))        
             
     scores_reg = scores_reg.reshape(len(scores_reg),num_targets);
-    scores_domain = np.concatenate(list(scores_domain.values()),axis=1);
-    scores_domain = scores_domain.reshape(len(scores_domain),num_labels_domain);
+    scores_domain = torch.zeros(scores_cat.shape[0],num_labels_domain);
     scores = np.concatenate((scores_cat,scores_reg,scores_domain),axis=1)
     gc.collect();
     return total_loss / num_batches, scores, labels_cat, targets, labels_domain, observers
