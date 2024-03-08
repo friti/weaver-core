d3b1f7f48157e32b8628b59aab8601a610ef4553
Raffaele <raffaele.gerosa@cern.ch>
2024-03-08 16:53:19 +0100

    bug fix

--- weaver/utils/dataset.py
+++ weaver/utils/dataset.py
@@ -149,7 +149,8 @@ def _preprocess(table, data_config, options):
         table, data_config.selection if options['training'] else data_config.test_time_selection,
         funcs=data_config.var_funcs)
     if len(table) == 0:
-        return _ , _
+        return {} , []
+    
     # define new variables
     aux_branches = data_config.train_aux_branches if options['training'] else data_config.test_aux_branches
     table = _build_new_variables(table, {k: v for k, v in data_config.var_funcs.items() if k in aux_branches})
