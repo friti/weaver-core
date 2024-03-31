045ba6a73406a753c8d0c45805288142a3550b1a
Raffaele <raffaele.gerosa@cern.ch>
2024-03-31 20:16:51 +0200

    bug fix

--- weaver/train.py
+++ weaver/train.py
@@ -459,7 +459,8 @@ def onnx(args):
     _logger.info('ONNX model saved to %s', args.export_onnx)
 
     preprocessing_json = os.path.join(os.path.dirname(args.export_onnx),args.export_onnx.replace(".onnx",".json"))
-    data_config.export_json(preprocessing_json,args.network_config.get("add_da_inference",False))
+    network_options = {k: ast.literal_eval(v) for k, v in args.network_option}
+    data_config.export_json(preprocessing_json,network_options.get("add_da_inference",False))
     _logger.info('Preprocessing parameters saved to %s', preprocessing_json)
 
 
